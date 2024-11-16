6f8eb2b8c8dea24c6d82990481b8eefa17a481be
Gilbert Koomson <gilbertkoomson00@gmail.com>
2024-11-15 23:51:39 -0800

    done the layout

---
+++ index.js
@@ -0,0 +1,7 @@
+function scrollFormIntoView(params) {
+    const form=document.getElementById('form');
+    form.scrollIntoView();
+    const input=document.getElementById('inputName');
+    input.focus()
+
+}
